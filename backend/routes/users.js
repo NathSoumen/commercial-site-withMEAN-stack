dd3a3b0d1e60933ea9fcba799db391cc081c5178
Soumen Nath <saumennath29@gmail.com>
2021-11-15 22:41:23 +0530

    expanding controller

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 const customerController = require('../controller/customerController')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/user/signup', function(req, res, next) {
+  customerController.signup(req,res,next)
+  // res.send('respond with a resource');
+});
+router.post('/user/checkEmail', function(req, res, next) {
+  customerController.checkEmail(req,res,next)
+  // res.send('respond with a resource');
+});
+router.post('/user/login', function(req, res, next) {
   customerController.login(req,res,next)
   // res.send('respond with a resource');
 });
