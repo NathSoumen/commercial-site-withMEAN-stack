831accd6598bcccc69c53caa357f4955b0d2c536
Soumen Nath <saumennath29@gmail.com>
2021-11-16 19:26:52 +0530

    add jwt on login

--- backend/controller/customerController.js
+++ backend/controller/customerController.js
@@ -1,42 +1,94 @@
-const customerModel = require('../model/customerAuth')
-
+const customerModel = require("../model/customerAuth");
+const jwt = require("jsonwebtoken");
 // signup
-exports.signup = function(req,res,next) {    
-
-    let newUser  = new customerModel();
-    newUser.username = req.body.username;
-    newUser.email = req.body.email;
-    newUser.mobile = req.body.mobile;
-    newUser.country = req.body.country;
-    newUser.password = newUser.hassPass(req.body.password);
-    newUser.save().then(() => {
-        res.status(200).send({success:true, msg:"user added"})
-    }).catch((err) => {
-        res.status(200).send({success:false, err:err})
-
-    })   
-
-
-}
+exports.signup = function (req, res, next) {
+  let newUser = new customerModel();
+  newUser.username = req.body.username;
+  newUser.email = req.body.email;
+  newUser.mobile = req.body.mobile;
+  newUser.country = req.body.country;
+  newUser.password = newUser.hassPass(req.body.password);
+  newUser
+    .save()
+    .then(() => {
+      res.status(200).send({ success: true, msg: "user added" });
+    })
+    .catch((err) => {
+      res.status(200).send({ success: false, err: err });
+    });
+};
 // login
 
-exports.login = function(req,res,next) {    
-
-     
-}
-// check if email is exists
-exports.checkEmail = async function(req,res,next) {    
-    console.log(req.body)
-    customerModel.findOne({email:req.body.email}).then((user) => {
-        if(user) {
-            res.status(200).send({success:false, msg:`this ${email} already exists`})
-              
+exports.login = function (req, res, next) {
+  if (req.body.email) {
+    customerModel
+      .findOne({ email: req.body.email })
+      .then((user) => {
+        if (user) {
+          let result = user.comPass(req.body.password, user.password);
+          if (result === true) {
+            let token = jwt.sign(
+              {
+                data: {
+                  email: user.email,
+                  username: user.username,
+                  country: user.country,
+                  mobile: user.mobile
+                },
+              },
+              process.env.JWT_SECRET,
+              { expiresIn: "5 minutes" }
+            );
+            if (token) {
+              res
+                .status(200)
+                .send({
+                  success: true,
+                  msg: "user logged In",
+                  user: user,
+                  token: token,
+                });
+            } else {
+              res.status(200).send({ success: false, err: "error" });
+            }
+          } else {
+            res
+              .status(200)
+              .send({ success: false, msg: "email or password is not valid" });
+          }
         } else {
-            res.status(200).send({success:true, msg:`valid email`})
-
+          res.status(200).send({ success: false, msg: "email is not found" });
         }
-    }).catch((err) => {
-        res.status(200).send({success:false, err:err})
-
+      })
+      .catch((err) => {
+        res.status(200).send({ success: false, err: err });
+      });
+  }
+};
+// check if email is exists
+exports.checkEmail = async function (req, res, next) {
+  console.log(req.body);
+  customerModel
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        res
+          .status(200)
+          .send({
+            success: false,
+            msg: `this ${req.body.email} already exists`,
+          });
+      } else {
+        res.status(200).send({ success: true, msg: `valid email` });
+      }
     })
-}
+    .catch((err) => {
+      res
+        .status(200)
+        .send({
+          success: false,
+          err: err,
+          msg: `this ${req.body.email} already exists`,
+        });
+    });
+};
